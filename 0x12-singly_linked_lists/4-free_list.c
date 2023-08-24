ca49d2c5432dac4b806dc50a0371485e29202229
Thomas Otieno <itstslenses@gmail.com>
2023-08-24 15:42:36 -0700

    Singly linked

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include "lists.h"
 
-/**
- *  * free_list - frees a linked list
- *   * @head: list_t list to be freed
- *    */
+/*
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
 void free_list(list_t *head)
 {
 	 list_t *temp;
@@ -15,5 +15,5 @@ void free_list(list_t *head)
 			     free(head->str);
 			      free(head);
 			       head = temp;
-			        }
+                   }
 }
